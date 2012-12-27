e7dcd50f03573ff63e118219f3046ddfa3fcd201
Bjørn Remseth <la3lma@gmail.com>
2012-12-27 01:26:26 +0100

    Refactor to do service mapping in a separate class.

---
+++ blobee/src/test/java/no/rmz/blobee/ServiceAnnotationMapper.java
@@ -0,0 +1,57 @@
+package no.rmz.blobee;
+
+import com.google.common.base.Function;
+import static com.google.common.base.Preconditions.checkNotNull;
+import com.google.protobuf.Descriptors.MethodDescriptor;
+import com.google.protobuf.Descriptors.ServiceDescriptor;
+import com.google.protobuf.Message;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class ServiceAnnotationMapper {
+    private static final Logger log = Logger.getLogger(ServiceAnnotationMapper.class.getName());
+
+    private ServiceAnnotationMapper() {
+    }
+
+    // XXX This is obviously just a stub.
+    
+    public static void bindServices(final Object implementation, final RChannel rchannel) throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        checkNotNull(implementation);
+        for (final Method method : implementation.getClass().getMethods()) {
+            if (method.isAnnotationPresent(ProtobufRpcImplementation.class)) {
+                final ProtobufRpcImplementation annotation = method.getAnnotation(ProtobufRpcImplementation.class);
+                // Then find the MethodDescriptor for the
+                // service class
+                final Class serviceClass = annotation.serviceClass();
+                final Method getDescriptor = serviceClass.getMethod("getDescriptor");
+                final ServiceDescriptor serviceDescriptor = (ServiceDescriptor) getDescriptor.invoke(null);
+                final MethodDescriptor methodDesc = serviceDescriptor.findMethodByName(annotation.method());
+                // Then use that method descriptor to create a proper wrapper
+                // for the implementation.
+                final Function<Message, Message> wrapper = new Function<Message, Message>() {
+                    @Override
+                    public Message apply(final Message input) {
+                        try {
+                            return (Message) method.invoke(implementation, input);
+                        }
+                        catch (IllegalAccessException ex) {
+                            log.log(Level.SEVERE, null, ex);
+                        }
+                        catch (IllegalArgumentException ex) {
+                            log.log(Level.SEVERE, null, ex);
+                        }
+                        catch (InvocationTargetException ex) {
+                            log.log(Level.SEVERE, null, ex);
+                        }
+                        throw new RuntimeException("This is bad");
+                    }
+                };
+                rchannel.add(methodDesc, wrapper);
+            }
+        }
+    }
+
+}
--- blobee/src/test/java/no/rmz/blobee/SimpleInvocationTest.java
+++ blobee/src/test/java/no/rmz/blobee/SimpleInvocationTest.java
@@ -13,6 +13,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import no.rmz.blobeeproto.api.proto.Rpc;
 import no.rmz.blobeeproto.api.proto.Rpc.RpcParam;
+// import static com.google.common.base.Preconditions.checkNotNull;
 import no.rmz.blobeeproto.api.proto.Rpc.RpcResult;
 import no.rmz.blobeeproto.api.proto.Rpc.RpcService;
 import org.junit.Before;
@@ -33,51 +34,12 @@ public final class SimpleInvocationTest {
     public void setUp() throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         rchannel = new RChannel();
 
-        final SampleServerImpl ssi = new SampleServerImpl();
-
-        for (final Method method : ssi.getClass().getMethods()) {
-            if (method.isAnnotationPresent(ProtobufRpcImplementation.class)) {
-                final ProtobufRpcImplementation annotation =
-                        method.getAnnotation(ProtobufRpcImplementation.class);
-
-                // Then find the MethodDescriptor for the
-                // service class
-
-                final Class serviceClass = annotation.serviceClass();
-                final Method getDescriptor = serviceClass.getMethod("getDescriptor");
-                final ServiceDescriptor serviceDescriptor = (ServiceDescriptor) getDescriptor.invoke(null);
-                final MethodDescriptor methodDesc = serviceDescriptor.findMethodByName(annotation.method());
-
-                // Then use that method descriptor to create a proper wrapper
-                // for the implementation.
-
-                final Function<Message, Message> wrapper =
-                        new Function<Message, Message> () {
-                        @Override
-                        public Message apply(final Message input) {
-                                try {
-                                    return (Message) method.invoke(ssi, input);
-                                }
-                                catch (IllegalAccessException ex) {
-                                    log.log(Level.SEVERE, null, ex);
-                                }
-                                catch (IllegalArgumentException ex) {
-                                    log.log(Level.SEVERE, null, ex);
-                                }
-                                catch (InvocationTargetException ex) {
-                                    log.log(Level.SEVERE, null, ex);
-                                }
-                                throw new RuntimeException("This is bad");
-                         }
-                     };
-
-                rchannel.add(methodDesc, wrapper);
-            }
-        }
+        final SampleServerImpl implementation = new SampleServerImpl();
+        ServiceAnnotationMapper.bindServices(implementation, rchannel);
     }
 
     @Test
-        public void testRpcStuff() {
+    public void testRpcStuff() {
         final ServiceDescriptor descriptor = Rpc.RpcService.getDescriptor();
 
 
@@ -90,7 +52,7 @@ public final class SimpleInvocationTest {
                     org.junit.Assert.assertEquals(Rpc.StatusCode.HANDLER_FAILURE, response);
                 } else {
                     log.info("Oops, there was an error: "
-                             + controller.errorText());
+                            + controller.errorText());
                     org.junit.Assert.fail("There shouldn't be any errors");
                 }
             }
