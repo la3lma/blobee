ff4732ddc4d24ad503cb856d68a5b052a81d37f8
Bjørn Remseth <la3lma@gmail.com>
2013-01-17 14:14:52 +0100

    Removing unused code.

--- blobee/src/main/java/no/rmz/blobee/rpc/ServiceAnnotationMapper.java
+++ blobee/src/main/java/no/rmz/blobee/rpc/ServiceAnnotationMapper.java
@@ -1,13 +1,10 @@
 package no.rmz.blobee.rpc;
 
-import com.google.common.base.Function;
 import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.protobuf.Descriptors.MethodDescriptor;
 import com.google.protobuf.Descriptors.ServiceDescriptor;
-import com.google.protobuf.Message;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public final class ServiceAnnotationMapper {
@@ -24,71 +21,12 @@ public final class ServiceAnnotationMapper {
         checkNotNull(methodName);
         checkNotNull(serviceInterface);
 
-        
+
         final Method getDescriptor = serviceInterface.getMethod("getDescriptor");
         final ServiceDescriptor serviceDescriptor =
                 (ServiceDescriptor) getDescriptor.invoke(null);
         final MethodDescriptor methodDesc =
                 serviceDescriptor.findMethodByName(methodName);
         return methodDesc;
-    }
-
-    // XXX This is obviously just a stub.
-    //     A lot more checking should be done here, including
-    //     typechecking that is done at startup (semi-static ;-)
-    public static void bindServices(
-            final Object implementation,
-            final MethodMap rchannel)
-            throws NoSuchMethodException,
-            IllegalAccessException,
-            IllegalArgumentException,
-            InvocationTargetException {
-        checkNotNull(implementation);
-        for (final Method method : implementation.getClass().getMethods()) {
-            if (method.isAnnotationPresent(ProtobufRpcImplementation.class)) {
-                final ProtobufRpcImplementation annotation =
-                        method.getAnnotation(ProtobufRpcImplementation.class);
-                // Then find the MethodDescriptor for the
-                // service class
-
-                final Class serviceClass = annotation.serviceClass();
-                 /*
-                final Method getDescriptor = serviceClass.getMethod("getDescriptor");
-                final ServiceDescriptor serviceDescriptor =
-                        (ServiceDescriptor) getDescriptor.invoke(null);
-                final MethodDescriptor methodDesc =
-                        serviceDescriptor.findMethodByName(annotation.method());
-                */
-
-                final MethodDescriptor methodDesc =
-                        getMethodDescriptor(serviceClass,annotation.method());
-                // Then use that method descriptor to create a proper wrapper
-                // for the implementation.
-                final Function<Message, Message> wrapper =
-                        new Function<Message, Message>() {
-                            @Override
-                            public Message apply(final Message input) {
-                                try {
-                                    return (Message) method.invoke(implementation, input);
-                                }
-                                catch (IllegalAccessException ex) {
-                                    log.log(Level.SEVERE, null, ex);
-                                }
-                                catch (IllegalArgumentException ex) {
-                                    log.log(Level.SEVERE, null, ex);
-                                }
-                                catch (InvocationTargetException ex) {
-                                    log.log(Level.SEVERE, null, ex);
-                                }
-                                throw new RuntimeException("This is bad");
-                            }
-                        };
-
-                // XXX Duplicates, locking and all the rest are
-                //     ignored here.
-                // Finally add the new implementation
-                rchannel.add(methodDesc, wrapper);
-            }
-        }
     }
 }
